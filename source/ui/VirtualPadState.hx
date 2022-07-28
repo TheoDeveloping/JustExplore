6cbaebaddc6efa62808db7ef3212d7320281713d
TheoDeveloper <97988283+TheoPortz@users.noreply.github.com>
2022-07-27 23:33:31 -0600

    webiwabo

---
+++ source/ui/VirtualPadState.hx
@@ -0,0 +1,16 @@
+package ui;
+
+import flixel.ui.FlxVirtualPad;
+
+class VirtualPadState extends FlxVirtualPad
+{
+    public var virtualPad:FlxVirtualPad;
+    
+    public function new()
+        {
+            virtualPad = new FlxVirtualPad(FlxDPadMode.FULL, FlxActionMode.NONE);
+		    add(virtualPad);
+		    
+		    super();
+        }
+}
